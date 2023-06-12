b9e83a7ab64f3c5f127f177b080a6f8c5cc95c76
yimajo <y.imajo@gmail.com>
2023-06-12 14:27:56 +0900

    Create FeatureAView.swift

---
+++ Samples/SampleiOSApp/FeatureA/FeatureAView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+import SDWebImage
+
+struct FeatureAView: View {
+    var body: some View {
+        Text("A")
+    }
+}
+
+struct FeatureAView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureAView()
+    }
+}

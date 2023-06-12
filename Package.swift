45cb65c32ec31e69c815214855390f28c2b445ce
Yoshinori Imajo <y.imajo@gmail.com>
2023-06-12 11:19:52 +0900

    Update Package.swift
    Temporarily lower GitHub Actions to run on macOS12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "XcodeTargetGraphGen",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v12)
     ],
     products: [
         .executable(name: "xcgraphgen", targets: ["XcodeTargetGraphGen"])
